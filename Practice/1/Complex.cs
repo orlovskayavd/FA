596c4951ef9680eb64b8b7679aa2af532f038be0
orlovskayavd <65015047+orlovskayavd@users.noreply.github.com>
2020-07-07 11:30:22 +0300

    Add files via upload

---
+++ Practice/1/Complex.cs
@@ -0,0 +1,266 @@
+﻿#pragma warning disable CS0659 // Тип переопределяет Object.Equals(object o), но не переопределяет Object.GetHashCode()
+
+using _1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    public class Complex : ICloneable, IComparable, IComparable<Complex>, IEquatable<Complex>
+    {
+        private readonly Fraction _real;
+        private readonly Fraction _imaginary;
+
+        private readonly Fraction EPS = new Fraction(1, 100);
+        private readonly Fraction PI = new Fraction(22, 7);
+         
+        public Complex(Fraction real, Fraction imag)
+        {
+            if (real is null)
+            {
+                throw new ArgumentNullException(nameof(real));
+            }
+            if (imag is null)
+            {
+                throw new ArgumentNullException(nameof(imag));
+            }
+
+            _real = real;
+            _imaginary = imag;
+        }
+
+        public Complex(Fraction real) : this(real, new Fraction(0)) { }
+
+      
+        public Complex(Complex other) : this(other._real, other._imaginary) { }
+
+        public override string ToString()
+        {
+            return $"({_real.ToString(5)}) + ({_imaginary.ToString(5)})i";
+        }
+
+        #region Interfaces
+
+        public object Clone()
+        {
+            return new Complex(_real, _imaginary);
+        }
+
+        #region Equals
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+                throw new ArgumentNullException(nameof(obj));
+
+            if (obj is Complex other)
+                return Equals(other);
+
+            return false;
+        }
+
+        public bool Equals(Complex other)
+        {
+            if (other is null)
+                throw new ArgumentNullException(nameof(other));
+
+            return (_real == other._real && _imaginary == other._imaginary);
+        }
+
+        #endregion
+
+        #region CompareTo
+
+        public int CompareTo(object obj)
+        {
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+            if (obj is Complex other)
+            {
+                return CompareTo(other);
+            }
+
+            throw new ArgumentException("invalid type");
+        }
+
+        public int CompareTo(Complex other)
+        {
+            if (other is null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+
+            if (Equals(other))
+            {
+                return 0;
+            }
+            else if (_real > other._real)
+            {
+                return 1;
+            }
+            else if (_real < other._real)
+            {
+                return -1;
+            }
+            else if (_imaginary > other._imaginary)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Arithmetics
+
+        private static Complex SumOrSub(Complex first, Complex second, Func<Fraction, Fraction, Fraction> operation)
+        {
+            if (first is null)
+            {
+                throw new ArgumentNullException(nameof(first));
+            }
+            if (second is null)
+            {
+                throw new ArgumentNullException(nameof(second));
+            }
+
+            return new Complex(operation(first._real, second._real), operation(first._imaginary, second._imaginary));
+        }
+
+        private static Complex Multiply(Complex first, Complex second)
+        {
+            if (first is null)
+            {
+                throw new ArgumentNullException(nameof(first));
+            }
+            if (second is null)
+            {
+                throw new ArgumentNullException(nameof(second));
+            }
+
+            return new Complex(first._real * second._real - first._imaginary * second._imaginary,
+                first._real * second._imaginary + first._imaginary * second._real);
+        }
+
+        private static Complex Divide(Complex first, Complex second)
+        {
+            if (first is null)
+            {
+                throw new ArgumentNullException(nameof(first));
+            }
+            if (second is null)
+            {
+                throw new ArgumentNullException(nameof(second));
+            }
+
+            return new Complex((first._real * second._real + first._imaginary * second._imaginary) /
+                               (second._real.Pow(2) + second._imaginary.Pow(2)),
+                        (first._imaginary * second._real - first._real * second._imaginary) /
+                                (second._real.Pow(2) + second._imaginary.Pow(2)));
+        }
+
+        public Fraction Abs()
+        {
+            return (_real.Pow(2) + _imaginary.Pow(2)).Sqrt(EPS);
+        }
+
+        public Fraction Argument()
+        {
+            Fraction arg = new Fraction(_imaginary / _real);
+            var x = _imaginary / _real;
+            Fraction ptr = new Fraction(-1);
+            Fraction sum = new Fraction(0);
+
+            //невозможно разложить в ряд тангенс значение = 1
+            if (x.Abs() == new Fraction(1))
+                throw new ArithmeticException("There is no tg(x)");
+
+            if (x.Abs() > new Fraction(1))
+            {
+                x = x.Flip();
+                arg = x;
+
+                for (int i = 3; i < Int32.MaxValue - 2; i += 2)
+                {
+                    arg += ptr * x.Pow(i) / i;
+                    if ((arg.Abs() - sum.Abs()).Abs() < EPS)
+                        break;
+                    sum = arg;
+                    ptr = ptr * (-1);
+                }
+
+                return new Fraction((PI / 2 - arg));
+            }
+
+            for (int i = 3; i < Int32.MaxValue - 2; i += 2)
+            {
+                arg += ptr * x.Pow(i) / i;
+                if ((arg.Abs() - sum.Abs()).Abs() < EPS)
+                    break;
+                sum = arg;
+                ptr = ptr * (-1);
+            }
+
+            return arg;
+            
+        }
+
+        public Complex[] SqrtN(int pow)
+        {
+            if (pow < 0)
+                throw new ArgumentException(nameof(pow));
+
+            Complex[] arr = new Complex[pow];
+
+            for (int i = 0; i < pow; i++)
+            {
+                arr[i] = new Complex((this.Abs().Sqrt(EPS, pow) + PI * 2 * i) * (this.Argument() / pow).Cos(EPS),
+                    (this.Abs().Sqrt(EPS, pow) + PI * 2 * i) * (this.Argument() / pow).Sin(EPS));
+            }
+
+            return arr;
+        }
+
+        public Complex Pow(int pow)
+        {
+            if (pow < 0)
+                throw new ArgumentException("Возведение в отрицательную степень невозможно");
+            return new Complex(this.Abs().Pow(pow) * (this.Argument() * pow).Cos(EPS),
+                              this.Abs().Pow(pow) * (this.Argument() * pow).Sin(EPS));
+        }
+
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return SumOrSub(a, b, (Fraction x, Fraction y) => x + y);
+        }
+
+        public static Complex operator -(Complex a, Complex b)
+        {
+            return SumOrSub(a, b, (Fraction x, Fraction y) => x - y);
+        }
+
+        public static Complex operator *(Complex first, Complex second)
+        {
+            return Multiply(first, second);
+        }
+
+        public static Complex operator /(Complex first, Complex second)
+        {
+            return Divide(first, second);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Practice/1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using Fractions;
+using _1;
+
+namespace Complex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction(2, -6);
+            Fraction f2 = new Fraction(1, 2);
+            Complex a = new Complex(f1, f2);
+            Complex b = new Complex(f2, f1);
+            Console.WriteLine("First is {0} \n", a);
+            Console.WriteLine("Second is {0} \n", b);
+            Console.WriteLine("Sum: a + b is {0} \n", a + b);
+            Console.WriteLine("Sub: a - b is {0} \n", a - b);
+            Console.WriteLine("Mult: a * b is {0} \n", a * b);
+
+            try
+            {
+                Console.WriteLine("Div: a / b is {0} \n", a / b);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("First^2 is {0} \n", a.Pow(2));
+            Console.WriteLine("First arg is {0} \n", b, b.Argument().ToString(20));
+
+            Console.WriteLine("Abs: {0} = {1}", a, a.Abs().ToString(10));
+            //Complex a = new Complex(new Fraction(1, -2), new Fraction(1, 8)), b = new Complex(new Fraction(1, 2), new Fraction(1, 6));
+
+            //Console.WriteLine("Sum: {0} + {1} = {2}", a, b, a + b);
+            //Console.WriteLine("Sub: {0} - {1} = {2}", a, b, a - b);
+            //Console.WriteLine("Mult: {0} * {1} = {2}", a, b, a * b);
+
+            //try
+            //{
+            //    Console.WriteLine("Div: {0} / {1} = {2}", a, b, a / b);
+            //}
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            //Console.WriteLine("Arg: {0} = {1}", b, b.Argument().ToString(20));
+            //Console.WriteLine("Abs: {0} = {1}", a, a.Abs().ToString(10));
+            //Console.WriteLine("Pow: ({0})^10 = {1}", a, a.Pow(10));
+
+            //var pow = 5;
+            //Complex[] arr = a.SqrtN(pow);
+
+            //try
+            //{
+            //    for (int i = 0; i < pow; i++)
+            //        Console.WriteLine("Sqrt number - {0}: {1}", i, arr[i]);
+            //}
+            //catch (ArgumentException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //Console.WriteLine("({0}).CompareTo({1}) = {2}", a, b, a.CompareTo(b));
+            //Console.WriteLine("Equals: {0} and {1} - {2}", a, b, a.Equals(b));
+        }
+    }
+}
